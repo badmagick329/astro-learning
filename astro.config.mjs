3f00081feb9c55b8573a0c113a15d5919a610e15
badmagick329 <uzairfarooqi@gmail.com>
2025-11-29 21:18:23 +0000

    add preact and fix formatting by switching to npm

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  site: "https://spiffy-tartufo-ea02ba.netlify.app/"
 
+  site: "https://spiffy-tartufo-ea02ba.netlify.app/",
+  integrations: [preact()]
 });
---
+++ src/components/Greeting.tsx
@@ -0,0 +1,26 @@
+import { useState } from "preact/hooks";
+
+export default function Greeting({ messages }: { messages: string[] }) {
+  const randomMessage = () => {
+    let newMsg = "";
+    do {
+      newMsg = messages[Math.floor(Math.random() * messages.length)];
+    } while (newMsg === greeting);
+
+    return newMsg;
+  };
+
+  const [greeting, setGreeting] = useState(messages[0]);
+
+  return (
+    <main className="mt-8 flex flex-col items-center gap-4">
+      <h3 className="text-4xl font-black">{greeting}</h3>
+      <button
+        className="bg-primary border-border hover:bg-primary/80 px-2 py-1 hover:cursor-pointer"
+        onClick={() => setGreeting(randomMessage())}
+      >
+        Generate new greeting
+      </button>
+    </main>
+  );
+}
